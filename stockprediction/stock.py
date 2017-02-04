a38aade6b0c0eb5dbc4cecdf5152f0c6cd25de1f
kevin25john <kevin25_john@yahoo.co.in>
2017-02-04 20:34:50 +0530

    Add files via upload

--- stockprediction/stock.py
+++ stockprediction/stock.py
@@ -3,22 +3,54 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
 import matplotlib.pyplot as plt
+import warnings
 
+from datetime import datetime
+#warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+import yahoo_finance
+from yahoo_finance import Share
+import pandas as pd
+
+
+name= input("enter the stock keyword  ")
+date_start = input("enter start date in the order YY-MM-DD  ")
+date_end = input("enter end date in the order YY-MM-DD  ")
+symbol = Share(name)
+#print(symbol.get_open())
+google_data = symbol.get_historical(date_start, date_end)
+google_df = pd.DataFrame(google_data)
+
+# Output data into CSV
+output_path = "D:\workspace"
+def make_filename(name, directory ="stockprediction"):
+    return output_path + "/" + directory + "/" + name +  ".csv"
+google_df.to_csv(make_filename(name,) )
+
+print("data received")
 dates = []
 prices = []
 
-def get_data(filename):
-    with open(filename, 'r') as csvfile:
+model = SVR(cache_size=7000)
+
+print("hello")
+
+def get_data(name):
+    with open(name, 'r') as csvfile:
         csvFileReader = csv.reader(csvfile)
         next(csvFileReader)
         for row in csvFileReader:
             dates.append(int(row[0].split('-')[0]))
+            print(dates)
             prices.append(float(row[1]))
         return
-    
+  
+  #
+  #gaurav anex, a-17, saraswat bank, near lajja show room. ground floor.  
+
 def predict_prices(dates, prices, x):
-    dates = np.reshape(dates, (len(dates)), 1)
-   
+    dates = np.reshape(dates, (len(dates), 1))  # @UndefinedVariable
+    
     svr_lin = SVR(kernel='linear', C=1e3)
     svr_poly = SVR(kernel = 'poly', C=1e3 , degree = 2 )
     svr_rbf = SVR(kernel = 'rbf', C=1e3 , gamma = 0.1)
@@ -33,10 +65,14 @@ def predict_prices(dates, prices, x):
     plt.xlabel('Dates')
     plt.ylabel('Price')
     plt.title('Support Vector Regresion')
+    plt.show()
     
     return svr_rbf.predict(x)[0], svr_lin.predict(x)[0], svr_poly.predict(x)[0]
 
-get_data('aapl.csv')
+data_name = name + ".csv"
 
-predicted_price =   predict_prices(dates, prices, 29)
-print(predicted_price) 
+get_data(data_name)
+
+predicted_price =   predict_prices(dates, prices, 26)
+print (predicted_price)
+print ("hello")
